cac51f54ad01903a2bda66f3c0ad7e96ac055ca4
catherine thy le <72633780+catherinethyle@users.noreply.github.com>
2022-05-15 20:34:19 -0700

    demo for prelim crit
    adding night and day mode option
    redesign object click and hover
    html and css design

--- sketch.js
+++ sketch.js
@@ -1,80 +1,119 @@
 let gameState = 'title';
 let font;
 let cardsDiscovery = false;
+let cardsResultsDisplay;
 let cootieDiscovery = false;
+let cootieResultsDisplay;
 let dsiDiscovery = false;
+let dsiResultsDisplay;
 let glCandyDiscovery = false;
+let glCandyResultsDisplay;
+let lockedResultsDisplay;
 let lpsDiscovery = false;
+let lpsResultsDisplay;
 let lycheeDiscovery = false;
+let lycheeResultsDisplay;
 let origamiStarDiscovery = false;
+let origamiStarResultsDisplay;
 let pcDiscovery = false;
+let pcResultsDisplay;
 let startTime = 0;
-let timeInt = 1800;
+let timeInt = 1800; //30sec
 let timeFrame;
-// let skyTheme;
+let skyTheme = true;
+let titleStarArchive = [];
 // let skyTheme = false;
 // let titleStarArchive[];
 
 function preload() {
-  aboutImg = loadImage('img/about.png');
-  aboutImg2 = loadImage('img/about2.png');
-  aboutScreenImg = loadImage('img/aboutscreen.png');
   bg = loadImage('img/bg.png');
   bg2 = loadImage('img/bg2.png');
   blankHolder = loadImage('img/blank.png');
   cardsImg = loadImage('img/cards.png');
   cardsHover = loadImage('img/cardshover.png');
+  cardsHoverNight = loadImage('img/cardshovernight.png');
   cardsClick = loadImage('img/cardsclick.png');
+  cardsNightImg = loadImage('img/cardsnight.png');
   cardsResults = loadImage('img/cardsresults.png');
+  cardsResultsNight = loadImage('img/cardsresultsnight.png');
   cootieImg = loadImage('img/cootie.png');
   cootieHover = loadImage('img/cootiehover.png');
+  cootieHoverNight = loadImage('img/cootiehovernight.png');
   cootieClick = loadImage('img/cootieclick.png');
+  cootieNightImg = loadImage('img/cootienight.png');
   cootieResults = loadImage('img/cootieresults.png');
+  cootieResultsNight = loadImage('img/cootieresultsnight.png');
   cornerImg = loadImage('img/logo.png');
   downImg = loadImage('img/down.png');
   downImg2 = loadImage('img/down2.png');
   dsiImg = loadImage('img/dsi.png');
   dsiClick = loadImage('img/dsiclick.png');
   dsiHover = loadImage('img/dsihover.png');
+  dsiHoverNight = loadImage('img/dsihovernight.png');
+  dsiNightImg = loadImage('img/dsinight.png');
   dsiResults = loadImage('img/dsiresults.png');
+  dsiResultsNight = loadImage('img/dsiresultsnight.png');
   endResultImg = loadImage('img/results.png');
   exitImg = loadImage('img/x.png');
   exitImg2 = loadImage('img/x2.png');
-  font = loadFont('sourceCodeProSB.ttf');
+  font = loadFont('txt/sourceCodeProSB.ttf');
   gameOverScreenImg = loadImage('img/gameoverscreen.png');
   gameOverImg = loadImage('img/gameover.png');
   gameOverImg2 = loadImage('img/gameover2.png');
   glCandyImg = loadImage('img/glcandy.png');
   glCandyClick = loadImage('img/glcandyclick.png');
   glCandyHover = loadImage('img/glcandyhover.png');
+  glCandyHoverNight = loadImage('img/glcandyhovernight.png');
+  glCandyNightImg = loadImage('img/glcandynight.png');
   glCandyResults = loadImage('img/glcandyresults.png');
+  glCandyResultsNight = loadImage('img/glcandyresultsnight.png');
   howToPlay = loadImage('img/instruction.png');
   lightingImg = loadImage('img/sunlight.png');
   lockedResults = loadImage('img/lockitem.png');
+  lockedResultsNight = loadImage('img/lockitemnight.png');
   lpsImg = loadImage('img/lps.png');
   lpsHover = loadImage('img/lpshover.png');
+  lpsHoverNight = loadImage('img/lpshover.png');
   lpsClick = loadImage('img/lpsclick.png');
+  lpsNightImg = loadImage('img/lpsnight.png');
   lpsResults = loadImage('img/lpsresults.png');
+  lpsResultsNight = loadImage('img/lpsresultsnight.png');
   lycheeImg = loadImage('img/lychee.png');
   lycheeHover = loadImage('img/lycheehover.png');
+  lycheeHoverNight = loadImage('img/lycheehovernight.png')
   lycheeClick = loadImage('img/lycheeclick.png');
+  lycheeNightImg = loadImage('img/lycheenight.png');
   lycheeResults = loadImage('img/lycheeresults.png');
+  lycheeResultsNight = loadImage('img/lycheeresultsnight.png');
   nextImg = loadImage('img/next.png');
   nextImg2 = loadImage('img/next2.png');
   origamiStarImg = loadImage('img/luckystar.png');
   origamiStarHover = loadImage('img/luckystarhover.png');
+  origamiStarHoverNight = loadImage('img/luckystarhovernight.png');
   origamiStarClick = loadImage('img/luckystarclick.png');
+  origamiStarNightImg = loadImage('img/luckystarnight.png');
   origamiStarResults = loadImage('img/luckystarresults.png')
+  origamiStarResultsNight = loadImage('img/luckystarresultsnight.png')
   pcImg = loadImage('img/pc.png');
   pcHover = loadImage('img/pchover.png');
+  pcHoverNight = loadImage('img/pchovernight.png');
   pcClick = loadImage('img/pcclick.png');
+  pcNightImg = loadImage('img/pcnight.png');
   pcResults = loadImage('img/pcresults.png');
+  pcResultsNight = loadImage('img/pcresultsnight.png');
   playAgainImg = loadImage('img/playagain.png');
   playAgainImg2 = loadImage('img/playagain2.png');
   skyDayImg = loadImage('img/skyday.png');
   skyDayCloud = loadImage('img/skydaycloud.png');
+  skyDayLightImg = loadImage('img/skydaylight.png');
+  skyDayLight2Img = loadImage('img/skydaylight2.png');
+  skyNightDesk1Img = loadImage('img/skynightupper.png');
+  skyNightDesk2Img = loadImage('img/skynightlower.png');
   skyNightImg = loadImage('img/skynight.png');
+  skyNightLightImg = loadImage('img/skynightlight.png');
+  skyNightLight2Img = loadImage('img/skynightlight2.png');
   skyNightMoon = loadImage('img/skynightmoonstar.png');
+  skyNightMoonThumbnail = loadImage('img/skynightmoon.png');
   skySelectionImg = loadImage('img/skyscreen.png');
   startImg = loadImage('img/start.png');
   startImg2 = loadImage('img/start2.png');
@@ -87,9 +126,11 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(650, 400);
+  let c = createCanvas(650, 400);
+  c.parent("p5sketch");
   frameRate(60); //set frameRate
   textFont(font);
+  angleMode(DEGREES);
 
   start = new Clickable();
   start.locate(525, 250);
@@ -125,7 +166,7 @@ function setup() {
     daySelection.imageScale = 1.2;
   }
   daySelection.onPress = function() {
-    // skyTheme = true;
+    skyTheme = true;
   }
   daySelection.onOutside = function() {
     daySelection.imageScale = 1.0;
@@ -136,7 +177,7 @@ function setup() {
   nightSelection.color = '#5b58a0';
   nightSelection.text = "";
   nightSelection.strokeWeight = 0;
-  nightSelection.image = skyNightMoon;
+  nightSelection.image = skyNightMoonThumbnail;
   nightSelection.fitImage = true;
   nightSelection.width = 150;
   nightSelection.height = 150;
@@ -144,7 +185,7 @@ function setup() {
     nightSelection.imageScale = 1.2;
   }
   nightSelection.onPress = function() {
-    // skyTheme = false;
+    skyTheme = false;
   }
   nightSelection.onOutside = function() {
     nightSelection.imageScale = 1.0;
@@ -264,6 +305,8 @@ function setup() {
   cards = new Clickable();
   cards.locate(200, 250);
   cards.color = '#d9d1a000';
+  cards.textSize = 3;
+  cards.textFont = font;
   cards.text = "";
   cards.strokeWeight = 0;
   cards.image = cardsImg;
@@ -271,9 +314,14 @@ function setup() {
   cards.width = 40;
   cards.height = 40;
   let cardsPopUp = cardsHover;
+  if (skyTheme) {
+    cardsPopUp = cardsHover;
+  } else {
+    cardsPopUp = cardsHoverNight;
+  }
   cards.onHover = function() {
     cards.imageScale = 1.2;
-    image(cardsPopUp, 75, 230); //125, 30
+    image(cardsPopUp, 0, 0); //125, 30
   }
   cards.onPress = function() {
     cardsPopUp = cardsClick;
@@ -294,9 +342,14 @@ function setup() {
   cootieCatcher.width = 50;
   cootieCatcher.hight = 50;
   let cootiePopUp = cootieHover;
+  if (skyTheme) {
+    cootiePopUp = cootieHover;
+  } else {
+    cootiePopUp = cootieHoverNight;
+  }
   cootieCatcher.onHover = function() {
     cootieCatcher.imageScale = 1.2;
-    image(cootiePopUp, 325, 250);
+    image(cootiePopUp, 0, 0);
   }
   cootieCatcher.onPress = function() {
     cootiePopUp = cootieClick;
@@ -316,9 +369,14 @@ function setup() {
   dsi.width = 80;
   dsi.height = 80;
   let dsiPopUp = dsiHover;
+  if (skyTheme) {
+    dsiPopUp = dsiHover;
+  } else {
+    dsiPopUp = dsiHoverNight;
+  }
   dsi.onHover = function() {
     dsi.imageScale = 1.2;
-    image(dsiPopUp, 425, 200);
+    image(dsiPopUp, 0, 0);
   }
   dsi.onPress = function() {
     dsiPopUp = dsiClick;
@@ -339,9 +397,14 @@ function setup() {
   goodLuckCandy.width = 30;
   goodLuckCandy.height = 30;
   let glCandyPopUp = glCandyHover;
+  if (skyTheme) {
+    glCandyPopUp = glCandyHover;
+  } else {
+    glCandyPopUp = glCandyHoverNight;
+  }
   goodLuckCandy.onHover = function() {
     goodLuckCandy.imageScale = 1.2;
-    image(glCandyPopUp, 200, 220);
+    image(glCandyPopUp, 0, 0);
   }
   goodLuckCandy.onPress = function() {
     glCandyPopUp = glCandyClick;
@@ -362,9 +425,14 @@ function setup() {
   lps.width = 50;
   lps.height = 50;
   let lpsPopUp = lpsHover;
+  if (skyTheme) {
+    lpsPopUp = lpsHover;
+  } else {
+    lpsPopUp = lpsHoverNight;
+  }
   lps.onHover = function() {
     lps.imageScale = 1.2;
-    image(lpsPopUp, 250, 180);
+    image(lpsPopUp, 0, 0);
   }
   lps.onPress = function() {
     lpsPopUp = lpsClick;
@@ -385,9 +453,14 @@ function setup() {
   lycheeJelly.width = 40;
   lycheeJelly.height = 40;
   let lycheePopUp = lycheeHover;
+  if (skyTheme) {
+    lycheePopUp = lycheeHover;
+  } else {
+    lycheePopUp = lycheeHoverNight;
+  }
   lycheeJelly.onHover = function() { //when on hover
     lycheeJelly.imageScale = 1.2;
-    image(lycheePopUp, 80, 240);
+    image(lycheePopUp, 0, 0);
   }
   lycheeJelly.onPress = function() { //when button pressed
     lycheePopUp = lycheeClick;
@@ -408,9 +481,14 @@ function setup() {
   pc.width = 200;
   pc.height = 200;
   pcPopUp = pcHover;
+  if (skyTheme) {
+    pcPopUp = pcHover;
+  } else {
+    pcPopUp = pcHoverNight;
+  }
   pc.onHover = function() {
     pc.imageScale = 1.2;
-    image(pcPopUp, 430, 180);
+    image(pcPopUp, 0, 0);
   }
   pc.onPress = function() {
     pcPopUp = pcClick;
@@ -431,9 +509,14 @@ function setup() {
   origamiStar.width = 50;
   origamiStar.height = 50;
   origamiStarPopUp = origamiStarHover;
+  if (skyTheme) {
+    origamiStarPopUp = origamiStarHover;
+  } else {
+    origamiStarPopUp = origamiStarHoverNight;
+  }
   origamiStar.onHover = function() {
     origamiStar.imageScale = 1.2;
-    image(origamiStarPopUp, 300, 175);
+    image(origamiStarPopUp, 0, 0);
   }
   origamiStar.onPress = function() {
     origamiStarPopUp = origamiStarClick;
@@ -494,39 +577,19 @@ function setup() {
     playAgain.image = playAgainImg;
     playAgain.imageScale = 1.0;
   }
-
-  about = new Clickable();
-  about.locate(350, 340);
-  about.color = '#d9d1a000';
-  about.text = "";
-  about.strokeWeight = 0;
-  about.image = aboutImg;
-  about.fitImage = true;
-  about.width = 90;
-  about.height = 30;
-  about.onHover = function() {
-    about.image = aboutImg2;
-    about.imageScale = 1.2;
-  }
-  about.onPress = function() {
-    gameState = 'aboutTheGame';
-  }
-  about.onOutside = function() {
-    about.image = aboutImg;
-    about.imageScale = 1.0;
-  }
 }
 
 function draw() {
-  // skyTheme = skyDayImg;
-  // if (skyTheme) {
-  //   image(skyDayImg, 0, 0);
-  // } else {
-  //   image(skyNightImg, 0, 0);
-  // }
-  // image(skyTheme, 0, 0);
-  image(skyDayImg, 0, 0);
-  background(bg);
+  if (skyTheme) {
+    image(skyDayImg, 0, 0);
+    background(bg);
+    image(skyDayLightImg, 0, 0);
+  } else {
+    image(skyNightImg, 0, 0);
+    image(skyNightMoon, 0, 0);
+    background(skyNightDesk1Img);
+    image(skyNightLightImg, 0, 0);
+  }
 
   //program wireframe
   switch (gameState) {
@@ -548,19 +611,19 @@ function draw() {
     case 'gameOver':
       gameOverScreen();
       break;
-    case 'aboutTheGame':
-      aboutScreen();
-      break;
     case 'endScreen':
       endScreen();
       break;
   }
 }
 
+
 function titleScreen() {
   background(titleImg);
   image(lightingImg, 0, 0);
   start.draw();
+  // titleStarArchive[].display();
+  // titleStarArchive[].move();
 }
 
 function skySelectionScreen() {
@@ -569,17 +632,59 @@ function skySelectionScreen() {
   daySelection.draw();
   nightSelection.draw();
   nextSky.draw();
+
+  if (skyTheme) {
+    start.image = pcImg;
+    nextSky.image = lycheeImg;
+    next.image = cootieImg;
+    exit.image = cardsImg;
+    up.image = dsiImg;
+    down.image = glCandyImg;
+    gameOver.image = lpsImg;
+    playAgain.image = origamiStarImg;
+  } else {
+    start.image = pcImg;
+    nextSky.image = lycheeImg;
+    next.image = cootieImg;
+    exit.image = cardsImg;
+    up.image = dsiImg;
+    down.image = glCandyImg;
+    gameOver.image = lpsImg;
+    playAgain.image = origamiStarImg;
+  }
 }
 
 function instructionScreen() {
   image(lightingImg, 0, 0);
   image(cornerImg, 0, 0);
   image(howToPlay, 0, 0);
+  textSize(18);
+  text("Welcome to Star Archive! Explore my desk area to take a walk down memory lane of what it is like growing up in the early 2000's in Southern California. Hover over objects to see what they are and click on them to read about there significance. Most importantly, have fun exploring >o<", 25, 135, 625, 400);
   next.draw();
   exit.draw();
 }
 
 function mainScreen() {
+  if (skyTheme) {
+    pc.image = pcImg;
+    lycheeJelly.image = lycheeImg;
+    cootieCatcher.image = cootieImg;
+    cards.image = cardsImg;
+    dsi.image = dsiImg;
+    goodLuckCandy.image = glCandyImg;
+    lps.image = lpsImg;
+    origamiStar.image = origamiStarImg;
+  } else {
+    pc.image = pcNightImg;
+    lycheeJelly.image = lycheeNightImg;
+    cootieCatcher.image = cootieNightImg;
+    cards.image = cardsNightImg;
+    dsi.image = dsiNightImg;
+    goodLuckCandy.image = glCandyNightImg;
+    lps.image = lpsNightImg;
+    origamiStar.image = origamiStarNightImg;
+  }
+
   pc.draw();
   lycheeJelly.draw();
   cootieCatcher.draw();
@@ -598,12 +703,23 @@ function mainScreen() {
 }
 
 function mainGameExpand() {
-  image(bg2, 0, 0);
+if (skyTheme) {
+  background(bg2);
+} else {
+  background(skyNightDesk2Img);
+}
+
   up.draw();
   image(cornerImg, 0, 0);
 
   if (frameCount > timeFrame) {
     gameState = 'gameOver';
+  }
+
+  if (skyTheme) {
+    image(skyDayLight2Img, 0, 0);
+  } else {
+    image(skyNightLight2Img, 0, 0);
   }
 }
 
@@ -615,66 +731,79 @@ function gameOverScreen() {
   gameOver.draw();
 }
 
-function aboutScreen() {
-  image(lightingImg, 0, 0);
-  image(cornerImg, 0, 0);
-  image(aboutScreenImg, 0, 0);
-  playAgain.draw();
-  gameOver.draw();
-}
-
 function endScreen() {
   image(lightingImg, 0, 0);
   image(cornerImg, 0, 0);
   image(endResultImg, 0, 0);
   playAgain.draw();
   // about.draw();
+  if (skyTheme) {
+    cardsResultsDisplay = cardsResults;
+    cootieResultsDisplay = cootieResults;
+    dsiResultsDisplay = dsiResults;
+    glCandyResultsDisplay = glCandyResults;
+    lockedResultsDisplay = lockedResults;
+    lpsResultsDisplay = lpsResults;
+    lycheeResultsDisplay = lycheeResults;
+    origamiStarResultsDisplay = origamiStarResults;
+    pcResultsDisplay = pcResults;
+  } else {
+    cardsResultsDisplay = cardsResultsNight;
+    cootieResultsDisplay = cootieResultsNight;
+    dsiResultsDisplay = dsiResultsNight;
+    glCandyResultsDisplay = glCandyResultsNight;
+    lockedResultsDisplay = lockedResultsNight;
+    lpsResultsDisplay = lpsResultsNight;
+    lycheeResultsDisplay = lycheeResultsNight;
+    origamiStarResultsDisplay = origamiStarResultsNight;
+    pcResultsDisplay = pcResultsNight;
+  }
 
   if (cardsDiscovery) {
-    image(cardsResults, 200, 140);
+    image(cardsResultsDisplay, 200, 140);
   } else {
-    image(lockedResults, 200, 140);
+    image(lockedResultsDisplay, 200, 140);
   }
 
   if (cootieDiscovery) {
-    image(cootieResults, 260, 140);
+    image(cootieResultsDisplay, 260, 140);
   } else {
-    image(lockedResults, 260, 140);
+    image(lockedResultsDisplay, 260, 140);
   }
 
   if (dsiDiscovery) {
-    image(dsiResults, 320, 140);
+    image(dsiResultsDisplay, 320, 140);
   } else {
-    image(lockedResults, 320, 140);
+    image(lockedResultsDisplay, 320, 140);
   }
 
   if (glCandyDiscovery) {
-    image(glCandyResults, 380, 140);
+    image(glCandyResultsDisplay, 380, 140);
   } else {
-    image(lockedResults, 380, 140);
+    image(lockedResultsDisplay, 380, 140);
   }
 
   if (lpsDiscovery) {
-    image(lpsResults, 200, 230);
+    image(lpsResultsDisplay, 200, 230);
   } else {
-    image(lockedResults, 200, 230);
+    image(lockedResultsDisplay, 200, 230);
   }
+
+    if (lycheeDiscovery) {
+      image(lycheeResultsDisplay, 320, 230);
+    } else {
+      image(lockedResultsDisplay, 320, 230);
+    }
 
   if (origamiStarDiscovery) {
-    image(origamiStarResults, 260, 230);
+    image(origamiStarResultsDisplay, 260, 230);
   } else {
-    image(lockedResults, 260, 230);
-  }
-
-  if (lycheeDiscovery) {
-    image(lycheeResults, 320, 230);
-  } else {
-    image(lockedResults, 320, 230);
+    image(lockedResultsDisplay, 260, 230);
   }
 
   if (pcDiscovery) {
-    image(pcResults, 380, 230);
+    image(pcResultsDisplay, 380, 230);
   } else {
-    image(lockedResults, 380, 230);
+    image(lockedResultsDisplay, 380, 230);
   }
 }
--- titleStarArchive.js
+++ titleStarArchive.js
@@ -1,16 +1,29 @@
 class TitleStarArchive {
-  constructor(xPos, yPos, scale) {
+  constructor(xPos, yPos, scale, rotation) {
     this.xPos = xPos;
     this.yPos = yPos;
     this.scale = scale;
+    this.rotation = rotation;
+    angleMode(DEGREES);
+  }
+
+  preload() {
+    titleStarImg = loadImg('img/title.png');
   }
 
   dispay() {
-    image(titleImg, this.xPos, this.yPos);
+    image(titleStarImg, this.xPos, this.yPos);
     scale(this.scale);
+    rotation(this.rotation);
   }
 
   move() {
-
+    if (this.rotation = 0) {
+      this.rotation = 20;
+    } else if (this.rotation > 20) {
+      this.rotation = -20
+    } else if (this.roation > -20) {
+      this.rotation = 0
+    }
   }
 }
